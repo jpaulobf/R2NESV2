4b4ed375b20d5e9c8b4a58d25f4c9dcbb0715643
jpaulobf <jpaulobf@gmail.com>
2026-05-01 16:13:54 +0100

    Initial structure

---
+++ src/launcher.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, R2NES v2!" << std::endl;
+    
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    std::cout << "The sum of " << a << " and " << b << " is " << sum << "." << std::endl;
+    
+    return 0;
+
+}
