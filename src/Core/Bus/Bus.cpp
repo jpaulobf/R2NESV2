4cb473757b258e4641e4d8d76985f71d339c1d8d
jpaulobf <jpaulobf@gmail.com>
2026-05-01 19:09:53 +0100

    Added Cartridge (ROM) to Bus

--- src/Core/Bus/Bus.cpp
+++ src/Core/Bus/Bus.cpp
@@ -1,5 +1,6 @@
 #include "Core/Bus/Bus.h"
 #include "Core/Memory/RAM/RAM.h"
+#include "Core/Cartridge/Cartridge.h"
 #include <algorithm>
 
 namespace R2NES::Core {
@@ -14,17 +15,28 @@ namespace R2NES::Core {
         this->ram = pRam;
     }
 
+    void Bus::setCartridge(const std::shared_ptr<Cartridge>& cartridge) {
+        this->cart = cartridge;
+    }
+
     void Bus::cpuWrite(uint16_t addr, uint8_t data) {
-        if (addr >= 0x0000 && addr <= 0x1FFF) {
+        if (cart && cart->cpuWrite(addr, data)) {
+            // Cartucho tratou a escrita (Mappers podem interceptar isso)
+        }
+        else if (addr >= 0x0000 && addr <= 0x1FFF) {
             if (ram) ram->write(addr & 0x07FF, data);
         }
     }
 
     uint8_t Bus::cpuRead(uint16_t addr, bool readOnly) {
-        if (addr >= 0x0000 && addr <= 0x1FFF) {
+        uint8_t data = 0x00;
+        if (cart && cart->cpuRead(addr, data)) {
+            // Cartucho tratou a leitura
+        }
+        else if (addr >= 0x0000 && addr <= 0x1FFF) {
             return ram ? ram->read(addr & 0x07FF) : 0x00;
         }
-        return 0x00;
+        return data;
     }
 
 }
--- src/Core/Bus/Bus.h
+++ src/Core/Bus/Bus.h
@@ -1,13 +1,27 @@
 #pragma once
 #include <cstdint>
+#include <memory>
 
 namespace R2NES::Core {
     class RAM;
+    class Cartridge;
 
     class Bus {
     public:
         Bus();
         ~Bus();
+
+        // Endereços dos Registradores da PPU (Memory Mapped I/O)
+        enum PPU_REGISTERS : uint16_t {
+            PPUCTRL   = 0x2000,
+            PPUMASK   = 0x2001,
+            PPUSTATUS = 0x2002,
+            OAMADDR   = 0x2003,
+            OAMDATA   = 0x2004,
+            PPUSCROLL = 0x2005,
+            PPUADDR   = 0x2006,
+            PPUDATA   = 0x2007
+        };
 
         // Conecta a RAM física ao barramento
         void connectRam(RAM* pRam);
@@ -16,7 +30,11 @@ namespace R2NES::Core {
         void cpuWrite(uint16_t addr, uint8_t data);
         uint8_t cpuRead(uint16_t addr, bool readOnly = false);
 
+        // Conecta o cartucho inserido
+        void setCartridge(const std::shared_ptr<Cartridge>& cartridge);
+
     public:
         RAM* ram = nullptr;
+        std::shared_ptr<Cartridge> cart;
     };
 }
