4f00527a256777e5afcb0799a866dd000e10144c
jpaulobf <jpaulobf@gmail.com>
2026-05-01 19:04:17 +0100

    Launcher

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include "Core/NESBoard.h" // Inclui o cabeçalho da NesBoard
 
-int main() {
-    std::cout << "Hello, R2NES v2!" << std::endl;
-    
-    int a = 5;
-    int b = 10;
-    int sum = a + b;
-    std::cout << "The sum of " << a << " and " << b << " is " << sum << "." << std::endl;
-    
+int main()
+{
+    std::cout << "Starting R2NES v2 Emulator..." << std::endl;
+
+    R2NES::Core::NesBoard nes; // Instancia a NesBoard
+
+    // Caminho para a ROM que você deseja testar
+    const std::string romPath = "D:\\Desenvolvimento\\Games\\R2NESV2\\roms\\smb.nes";
+    nes.insertCartridge(romPath); // Insere o cartucho
+
+    // O reset() já é chamado no construtor da NesBoard, mas chamá-lo novamente aqui
+    // garante que os testes de leitura da ROM no reset sejam executados APÓS o cartucho ser inserido.
+    nes.reset();
+
+    std::cout << "Emulator initialized. Press Enter to exit..." << std::endl;
+    std::cin.get(); // Mantém a janela aberta para ver a saída
     return 0;
-
 }
