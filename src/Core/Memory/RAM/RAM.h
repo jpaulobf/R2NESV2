bc80627205ff4282fc281b1b41f76727bf0ce97e
jpaulobf <jpaulobf@gmail.com>
2026-06-05 19:03:50 +0100

    QoL & SaveState (WIP)

--- src/Core/Memory/RAM/RAM.h
+++ src/Core/Memory/RAM/RAM.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <vector>
+#include <iostream>
 
 namespace R2NES::Core
 {
@@ -10,11 +11,18 @@ namespace R2NES::Core
         RAM(size_t size = 2048);
         ~RAM();
 
+        // Escrita de um byte na RAM
         void write(uint16_t addr, uint8_t data);
 
+        // Leitura de um byte da RAM
         uint8_t read(uint16_t addr) const;
 
+        // Retorna o tamanho da RAM
         size_t getSize() const { return data.size(); }
+
+        // Serialização para Save / Load states
+        void saveState(std::ostream &os);
+        void loadState(std::istream &is);
 
     private:
         std::vector<uint8_t> data;
