ef8be24f3a1feb0d439038d977adce8343757073
jpaulobf <jpaulobf@gmail.com>
2026-06-06 12:29:49 +0100

    QoL & SaveState (Done)

--- src/Core/Memory/VRAM/VRAM.h
+++ src/Core/Memory/VRAM/VRAM.h
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <array>
 #include "Common/Common.h"
+#include <iostream>
 
 namespace R2NES::Core
 {
@@ -14,6 +15,10 @@ namespace R2NES::Core
 
         uint8_t read(uint16_t addr, MirrorMode mode) const;
         void write(uint16_t addr, uint8_t data, MirrorMode mode);
+
+        // Serialização para Save / Load states
+        void saveState(std::ostream &os);
+        void loadState(std::istream &is);
 
     private:
         uint16_t getMirroredAddress(uint16_t addr, MirrorMode mode) const;
