20f4a00df3e62a9f3e67c76bc6c9db0e8aa11abb
jpaulobf <jpaulobf@gmail.com>
2026-06-07 11:26:15 +0100

    Palette Viewer & Palette Swapper (WIP)

--- src/Core/PPU/PPU.cpp
+++ src/Core/PPU/PPU.cpp
@@ -9,16 +9,63 @@ namespace R2NES::Core
     // Paleta de cores padrão do NES (64 cores ARGB)
     // Esta tabela converte o índice de cor (0-63) da Palette RAM em um valor de cor real.
     static const uint32_t nesSystemPalette[64] = {
-        0xFF545454, 0xFF001E74, 0xFF081090, 0xFF300088, 0xFF440064, 0xFF5C0030, 0xFF540400, 0xFF3C1800, 0xFF202A00, 0xFF083A00, 0xFF004000, 0xFF003C00, 0xFF003260, 0xFF000000, 0xFF000000, 0xFF000000,
-        0xFF989698, 0xFF084CC4, 0xFF3032EC, 0xFF5C1EE4, 0xFF8814B0, 0xFFA01468, 0xFFB03200, 0xFF8C4400, 0xFF546000, 0xFF207400, 0xFF087C00, 0xFF007A28, 0xFF006C90, 0xFF000000, 0xFF000000, 0xFF000000,
-        0xFFECEEE8, 0xFF4C9AEC, 0xFF787CEC, 0xFFB062EC, 0xFFE454EC, 0xFFEC58B4, 0xFFEC6A5C, 0xFFD48820, 0xFFA0AA00, 0xFF74C400, 0xFF4CD020, 0xFF38CC6C, 0xFF38B4CC, 0xFF3E3E3E, 0xFF000000, 0xFF000000,
-        0xFFFCFCFC, 0xFFA4D2FC, 0xFFB8B8FC, 0xFFD8A8FC, 0xFFF8A4FC, 0xFFF8A8D8, 0xFFF8B4B4, 0xFFF0C090, 0xFFD8D470, 0xFFC4E470, 0xFFB0EC90, 0xFFA4ECAF, 0xFFA4E2FC, 0xFFB8B8B8, 0xFF000000, 0xFF000000};
+        0xFF545454, 0xFF001E74, 0xFF081090, 0xFF300088, 0xFF440064, 0xFF5C0030, 0xFF540400, 0xFF3C1800,
+        0xFF202A00, 0xFF083A00, 0xFF004000, 0xFF003C00, 0xFF003260, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFF989698, 0xFF084CC4, 0xFF3032EC, 0xFF5C1EE4, 0xFF8814B0, 0xFFA01468, 0xFFB03200, 0xFF8C4400,
+        0xFF546000, 0xFF207400, 0xFF087C00, 0xFF007A28, 0xFF006C90, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFECEEE8, 0xFF4C9AEC, 0xFF787CEC, 0xFFB062EC, 0xFFE454EC, 0xFFEC58B4, 0xFFEC6A5C, 0xFFD48820,
+        0xFFA0AA00, 0xFF74C400, 0xFF4CD020, 0xFF38CC6C, 0xFF38B4CC, 0xFF3E3E3E, 0xFF000000, 0xFF000000,
+        0xFFFCFCFC, 0xFFA4D2FC, 0xFFB8B8FC, 0xFFD8A8FC, 0xFFF8A4FC, 0xFFF8A8D8, 0xFFF8B4B4, 0xFFF0C090,
+        0xFFD8D470, 0xFFC4E470, 0xFFB0EC90, 0xFFA4ECAF, 0xFFA4E2FC, 0xFFB8B8B8, 0xFF000000, 0xFF000000};
+
+    static const uint32_t PALETTE_SMOOTH[64] = {
+        0xFF6A6A6A, 0xFF001E8C, 0xFF0610A0, 0xFF2A009B, 0xFF4E007A, 0xFF5B0047, 0xFF570012, 0xFF450D00,
+        0xFF292200, 0xFF093300, 0xFF003D00, 0xFF003D17, 0xFF003754, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFB5B5B5, 0xFF004ADC, 0xFF2B36FA, 0xFF6320EF, 0xFF9514C3, 0xFFA91681, 0xFFA4253A, 0xFF8F3D00,
+        0xFF695600, 0xFF376E00, 0xFF147B00, 0xFF007A41, 0xFF007197, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFFFFFFF, 0xFF3C96FF, 0xFF6C81FF, 0xFFA76BFF, 0xFFDD5DFF, 0xFFF85CD2, 0xFFF66D81, 0xFFE1873A,
+        0xFFBAA206, 0xFF87BA00, 0xFF60C823, 0xFF45C874, 0xFF40BFD1, 0xFF454545, 0xFF000000, 0xFF000000,
+        0xFFFFFFFF, 0xFFA9D4FF, 0xFFBDCCFF, 0xFFD5C3FF, 0xFFEBBDFF, 0xFFF7BDEE, 0xFFF7C4CE, 0xFFEFCEB1,
+        0xFFDED99C, 0xFFC9E39C, 0xFFB9E9AB, 0xFFAEE9CC, 0xFFACE5F2, 0xFFAEAEAE, 0xFF000000, 0xFF000000};
+
+    static const uint32_t PALETTE_NESTOPIA[64] = {
+        0xFF7C7C7C, 0xFF0000FC, 0xFF0000BC, 0xFF4428BC, 0xFF940084, 0xFFA80020, 0xFFA81000, 0xFF881400,
+        0xFF503000, 0xFF007800, 0xFF006800, 0xFF005800, 0xFF004058, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFBCBCBC, 0xFF0078F8, 0xFF0058F8, 0xFF6844FC, 0xFFD800CC, 0xFFE40058, 0xFFF83800, 0xFFE45C10,
+        0xFFAC7C00, 0xFF00B800, 0xFF00A800, 0xFF00A844, 0xFF008888, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFF878F8, 0xFF3CBCFC, 0xFF68A0FC, 0xFFB488FC, 0xFFF878F8, 0xFFFD78B4, 0xFFF88444, 0xFFF89800,
+        0xFFE4D410, 0xFF58D854, 0xFF58F898, 0xFF00E8D8, 0xFF787878, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFFCFCFC, 0xFFA4E4FC, 0xFFB8B8F8, 0xFFD8B8F8, 0xFFF8B8F8, 0xFFF8A4C0, 0xFFF0D0B0, 0xFFFCE0A8,
+        0xFFF8D878, 0xFFD8F878, 0xFFB8F8B8, 0xFFB8F8D8, 0xFF00FCFC, 0xFFF8D8F8, 0xFF000000, 0xFF000000};
+
+    static const uint32_t PALETTE_WAVEBEAM[64] = {
+        0xFF525252, 0xFF00009C, 0xFF0000B5, 0xFF3100A5, 0xFF6B007B, 0xFF8B0039, 0xFF830000, 0xFF5A0C00,
+        0xFF292200, 0xFF003500, 0xFF003D00, 0xFF003518, 0xFF002A52, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFF9C9C9C, 0xFF0042F7, 0xFF0021F7, 0xFF5A00F7, 0xFFA500D6, 0xFFD60073, 0xFFCE1000, 0xFF9C3100,
+        0xFF5A4A00, 0xFF006300, 0xFF007300, 0xFF006B42, 0xFF005A9C, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFECEEEC, 0xFF319CF7, 0xFF527BF7, 0xFF9C5AF7, 0xFFED42F7, 0xFFF731BD, 0xFFF74A5A, 0xFFDE6B00,
+        0xFF9C8B00, 0xFF31A500, 0xFF00B521, 0xFF00AD73, 0xFF009CDE, 0xFF313131, 0xFF000000, 0xFF000000,
+        0xFFECEEEC, 0xFFADDEF7, 0xFFBDCEF7, 0xFFD6BDF7, 0xFFF7BDF7, 0xFFF7BDDE, 0xFFF7C6BD, 0xFFF7CE9C,
+        0xFFDEDE9C, 0xFFBDE79C, 0xFF9CEFBD, 0xFF9CEEDE, 0xFF9CE7F7, 0xFFBDBDBD, 0xFF000000, 0xFF000000};
+
+    static const uint32_t PALETTE_CYBER_NEON[64] = {
+        0xFF6A6A6A, 0xFF8C1E00, 0xFF0610A0, 0xFF9B002A, 0xFF7A004E, 0xFF47005B, 0xFF120057, 0xFF000D45,
+        0xFF002229, 0xFF003309, 0xFF003D00, 0xFF173D00, 0xFF543700, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFB5B5B5, 0xFFDC4A00, 0xFF2B36FA, 0xFFEF2063, 0xFFC31495, 0xFF8116A9, 0xFF3B25A4, 0xFF003D8F,
+        0xFF005669, 0xFF006E37, 0xFF007B14, 0xFF417A00, 0xFF977100, 0xFF000000, 0xFF000000, 0xFF000000,
+        0xFFFFFFFF, 0xFFFF963C, 0xFF6C81FF, 0xFFFF6BA7, 0xFFFF5DDD, 0xFFD25CF8, 0xFF816DF6, 0xFF3A87E1,
+        0xFF06A2BA, 0xFF00BA87, 0xFF23C860, 0xFF74C845, 0xD1BF40FF, 0xFF454545, 0xFF000000, 0xFF000000,
+        0xFFFFFFFF, 0xFFD4A9FF, 0xFFBDCCFF, 0xFFC3D5FF, 0xFFBDEBFF, 0xFFBDF7EE, 0xFFC4F7CE, 0xCEEFB1FF,
+        0xFF9CD9DE, 0xFF9CE3C9, 0xABE9B9FF, 0xCCE9AEFF, 0xF2E5ACFF, 0xFFAEAEAE, 0xFF000000, 0xFF000000};
+
+    // Inicialização da variável estática (feita uma única vez no escopo global)
+    const uint32_t *PPU::currentPalette = PALETTE_CYBER_NEON;
 
     PPU::PPU()
     {
         std::fill(paletteTable.begin(), paletteTable.end(), 0x00);
         std::fill(frameBuffer.begin(), frameBuffer.end(), 0xFF000000); // Inicializa com preto opaco
-        std::fill(oamMemory.begin(), oamMemory.end(), 0xFF); // Inicializa fora da tela (Y=255)
+        std::fill(oamMemory.begin(), oamMemory.end(), 0xFF);           // Inicializa fora da tela (Y=255)
 
         // Debug: Inicializa paletas com valores padrão para o Viewer funcionar sem ROM carregar paletas
         vramAddr = 0;
@@ -32,6 +79,36 @@ namespace R2NES::Core
 
     PPU::~PPU()
     {
+    }
+
+    const uint32_t *PPU::getSystemPalette()
+    {
+        return currentPalette;
+    }
+
+    void PPU::setSystemPalette(PaletteType type)
+    {
+        switch (type)
+        {
+        case PaletteType::DEFAULT:
+            currentPalette = nesSystemPalette;
+            break;
+        case PaletteType::SMOOTH:
+            currentPalette = PALETTE_SMOOTH;
+            break;
+        case PaletteType::NESTOPIA:
+            currentPalette = PALETTE_NESTOPIA;
+            break;
+        case PaletteType::WAVEBEAM:
+            currentPalette = PALETTE_WAVEBEAM;
+            break;
+        case PaletteType::NEON:
+            currentPalette = PALETTE_CYBER_NEON;
+            break;
+        default:
+            currentPalette = nesSystemPalette;
+            break;
+        }
     }
 
     void PPU::connectBus(Bus *bus)
@@ -253,7 +330,7 @@ namespace R2NES::Core
                         // Escreve no buffer de pixels na posição correta da imagem 128x128
                         uint32_t pixelX = tileX * 8 + col;
                         uint32_t pixelY = tileY * 8 + row;
-                        pixels[pixelY * 128 + pixelX] = nesSystemPalette[systemPaletteIndex];
+                        pixels[pixelY * 128 + pixelX] = currentPalette[systemPaletteIndex];
                     }
                 }
             }
@@ -321,7 +398,7 @@ namespace R2NES::Core
         if (scanline == -1 && cycle == 1)
         {
             // Limpa flags de VBlank, Sprite 0 Hit e Overflow no início do pre-render
-            ppuStatus &= ~0xE0; 
+            ppuStatus &= ~0xE0;
             sprite0HitDetectedThisScanline = false;
         }
 
@@ -431,7 +508,7 @@ namespace R2NES::Core
             uint16_t bgPaletteAddr = 0x3F00 + (bgPaletteIndex * 4) + bgPixelColor;
             if (bgPixelColor == 0)
                 bgPaletteAddr = 0x3F00;
-            frameBuffer[scanline * 256 + cycle] = nesSystemPalette[ppuRead(bgPaletteAddr) & 0x3F];
+            frameBuffer[scanline * 256 + cycle] = currentPalette[ppuRead(bgPaletteAddr) & 0x3F];
 
             // --- Renderização de Sprites (Otimizada para este ciclo) ---
             bool spriteRenderingEnabled = (ppuMask & 0x10) != 0;
@@ -536,7 +613,7 @@ namespace R2NES::Core
                                 if (i == 0 && usedDebugColors)
                                     frameBuffer[scanline * 256 + cycle] = 0xFFFF00FF;
                                 else
-                                    frameBuffer[scanline * 256 + cycle] = nesSystemPalette[ppuRead(palAddr) & 0x3F];
+                                    frameBuffer[scanline * 256 + cycle] = currentPalette[ppuRead(palAddr) & 0x3F];
 
                                 spritePixelDrawn = true;
                             }
@@ -591,7 +668,7 @@ namespace R2NES::Core
         // Implementação do Odd Frame Cycle Skip
         if (scanline == -1 && cycle == 339 && renderingEnabled && (frameCounter % 2 != 0))
         {
-            cycle = 340; 
+            cycle = 340;
         }
     }
 
@@ -620,42 +697,42 @@ namespace R2NES::Core
     void PPU::saveState(std::ostream &os)
     {
         vram.saveState(os);
-        os.write(reinterpret_cast<const char*>(&ppuCtrl), sizeof(ppuCtrl));
-        os.write(reinterpret_cast<const char*>(&ppuMask), sizeof(ppuMask));
-        os.write(reinterpret_cast<const char*>(&ppuStatus), sizeof(ppuStatus));
-        os.write(reinterpret_cast<const char*>(&oamAddr), sizeof(oamAddr));
-        os.write(reinterpret_cast<const char*>(&addressLatch), sizeof(addressLatch));
-        os.write(reinterpret_cast<const char*>(&vramAddr), sizeof(vramAddr));
-        os.write(reinterpret_cast<const char*>(&tempAddr), sizeof(tempAddr));
-        os.write(reinterpret_cast<const char*>(&fineX), sizeof(fineX));
-        os.write(reinterpret_cast<const char*>(&dataBuffer), sizeof(dataBuffer));
-        os.write(reinterpret_cast<const char*>(&scanline), sizeof(scanline));
-        os.write(reinterpret_cast<const char*>(&cycle), sizeof(cycle));
-        os.write(reinterpret_cast<const char*>(&frameCounter), sizeof(frameCounter));
-        os.write(reinterpret_cast<const char*>(&nmi), sizeof(nmi));
-        os.write(reinterpret_cast<const char*>(&sprite0HitDetectedThisScanline), sizeof(sprite0HitDetectedThisScanline));
-        os.write(reinterpret_cast<const char*>(oamMemory.data()), oamMemory.size());
-        os.write(reinterpret_cast<const char*>(paletteTable.data()), paletteTable.size());
+        os.write(reinterpret_cast<const char *>(&ppuCtrl), sizeof(ppuCtrl));
+        os.write(reinterpret_cast<const char *>(&ppuMask), sizeof(ppuMask));
+        os.write(reinterpret_cast<const char *>(&ppuStatus), sizeof(ppuStatus));
+        os.write(reinterpret_cast<const char *>(&oamAddr), sizeof(oamAddr));
+        os.write(reinterpret_cast<const char *>(&addressLatch), sizeof(addressLatch));
+        os.write(reinterpret_cast<const char *>(&vramAddr), sizeof(vramAddr));
+        os.write(reinterpret_cast<const char *>(&tempAddr), sizeof(tempAddr));
+        os.write(reinterpret_cast<const char *>(&fineX), sizeof(fineX));
+        os.write(reinterpret_cast<const char *>(&dataBuffer), sizeof(dataBuffer));
+        os.write(reinterpret_cast<const char *>(&scanline), sizeof(scanline));
+        os.write(reinterpret_cast<const char *>(&cycle), sizeof(cycle));
+        os.write(reinterpret_cast<const char *>(&frameCounter), sizeof(frameCounter));
+        os.write(reinterpret_cast<const char *>(&nmi), sizeof(nmi));
+        os.write(reinterpret_cast<const char *>(&sprite0HitDetectedThisScanline), sizeof(sprite0HitDetectedThisScanline));
+        os.write(reinterpret_cast<const char *>(oamMemory.data()), oamMemory.size());
+        os.write(reinterpret_cast<const char *>(paletteTable.data()), paletteTable.size());
     }
 
     void PPU::loadState(std::istream &is)
     {
         vram.loadState(is);
-        is.read(reinterpret_cast<char*>(&ppuCtrl), sizeof(ppuCtrl));
-        is.read(reinterpret_cast<char*>(&ppuMask), sizeof(ppuMask));
-        is.read(reinterpret_cast<char*>(&ppuStatus), sizeof(ppuStatus));
-        is.read(reinterpret_cast<char*>(&oamAddr), sizeof(oamAddr));
-        is.read(reinterpret_cast<char*>(&addressLatch), sizeof(addressLatch));
-        is.read(reinterpret_cast<char*>(&vramAddr), sizeof(vramAddr));
-        is.read(reinterpret_cast<char*>(&tempAddr), sizeof(tempAddr));
-        is.read(reinterpret_cast<char*>(&fineX), sizeof(fineX));
-        is.read(reinterpret_cast<char*>(&dataBuffer), sizeof(dataBuffer));
-        is.read(reinterpret_cast<char*>(&scanline), sizeof(scanline));
-        is.read(reinterpret_cast<char*>(&cycle), sizeof(cycle));
-        is.read(reinterpret_cast<char*>(&frameCounter), sizeof(frameCounter));
-        is.read(reinterpret_cast<char*>(&nmi), sizeof(nmi));
-        is.read(reinterpret_cast<char*>(&sprite0HitDetectedThisScanline), sizeof(sprite0HitDetectedThisScanline));
-        is.read(reinterpret_cast<char*>(oamMemory.data()), oamMemory.size());
-        is.read(reinterpret_cast<char*>(paletteTable.data()), paletteTable.size());
+        is.read(reinterpret_cast<char *>(&ppuCtrl), sizeof(ppuCtrl));
+        is.read(reinterpret_cast<char *>(&ppuMask), sizeof(ppuMask));
+        is.read(reinterpret_cast<char *>(&ppuStatus), sizeof(ppuStatus));
+        is.read(reinterpret_cast<char *>(&oamAddr), sizeof(oamAddr));
+        is.read(reinterpret_cast<char *>(&addressLatch), sizeof(addressLatch));
+        is.read(reinterpret_cast<char *>(&vramAddr), sizeof(vramAddr));
+        is.read(reinterpret_cast<char *>(&tempAddr), sizeof(tempAddr));
+        is.read(reinterpret_cast<char *>(&fineX), sizeof(fineX));
+        is.read(reinterpret_cast<char *>(&dataBuffer), sizeof(dataBuffer));
+        is.read(reinterpret_cast<char *>(&scanline), sizeof(scanline));
+        is.read(reinterpret_cast<char *>(&cycle), sizeof(cycle));
+        is.read(reinterpret_cast<char *>(&frameCounter), sizeof(frameCounter));
+        is.read(reinterpret_cast<char *>(&nmi), sizeof(nmi));
+        is.read(reinterpret_cast<char *>(&sprite0HitDetectedThisScanline), sizeof(sprite0HitDetectedThisScanline));
+        is.read(reinterpret_cast<char *>(oamMemory.data()), oamMemory.size());
+        is.read(reinterpret_cast<char *>(paletteTable.data()), paletteTable.size());
     }
 }
