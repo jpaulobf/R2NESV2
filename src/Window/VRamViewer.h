7f638f4ea7efdf946b951495c423e166f54c0571
jpaulobf <jpaulobf@gmail.com>
2026-06-08 12:46:33 +0100

    Mapper066 (WIP) and CPU/PPU Improvements

--- src/Window/VRamViewer.h
+++ src/Window/VRamViewer.h
@@ -3,18 +3,31 @@
 #include <SDL.h>
 #include <cstdint>
 #include <string>
+#include "imgui.h"
 
 namespace R2NES::Core
 {
+    class VRAM;
+
     class VRamViewer
     {
     public:
         VRamViewer();
         ~VRamViewer();
 
+        void open(int parentX, int parentY, int parentW);
+        void close();
+        void render(VRAM *vram);
+        void handleEvent(SDL_Event *e);
+        void updatePosition(int parentX, int parentY, int parentW);
+
+        bool isOpen() const { return visible; }
+        uint32_t getWindowID() const;
+
     private:
         SDL_Window *window = nullptr;
         SDL_Renderer *renderer = nullptr;
+        ImGuiContext *imguiContext = nullptr;
         
         bool visible = false;
     };
