6eb4ae6b95711d7886a867632593b6c08750aaae
jpaulobf <jpaulobf@gmail.com>
2026-06-08 12:46:20 +0100

    Mapper066 (WIP) and CPU/PPU Improvements

--- src/Core/Memory/VRAM/VRAM.h
+++ src/Core/Memory/VRAM/VRAM.h
@@ -13,8 +13,15 @@ namespace R2NES::Core
         VRAM();
         ~VRAM();
 
+        /* Limpa toda a memória das Name Tables. */
+        void reset();
+
         uint8_t read(uint16_t addr, MirrorMode mode) const;
         void write(uint16_t addr, uint8_t data, MirrorMode mode);
+
+        uint8_t readRaw(uint16_t addr) const { return vram[addr & 0x07FF]; }
+        void writeRaw(uint16_t addr, uint8_t data) { vram[addr & 0x07FF] = data; }
+        size_t getSize() const { return vram.size(); }
 
         // Serialização para Save / Load states
         void saveState(std::ostream &os);
