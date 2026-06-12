a2d6d9fe160620e2dd3c2ebe6ea290c8b38c6e92
jpaulobf <jpaulobf@gmail.com>
2026-06-12 23:49:59 +0100

    Reformat code

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -2,12 +2,12 @@
 #include <SDL.h>
 #include "Window/Engine.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     std::cout << "Starting R2NES v2 Emulator..." << std::endl;
-    
+
     R2NES::Core::Engine emulator;
     emulator.run();
-    
+
     return 0;
 }
