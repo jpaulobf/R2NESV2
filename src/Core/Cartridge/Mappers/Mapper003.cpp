63efacb72d3be9c941db2cad7b80850b363c114f
jpaulobf <jpaulobf@gmail.com>
2026-06-13 19:59:31 +0100

    Mapper4 improvements

--- src/Core/Cartridge/Mappers/Mapper003.cpp
+++ src/Core/Cartridge/Mappers/Mapper003.cpp
@@ -33,7 +33,7 @@ namespace R2NES::Core
         return false;
     }
 
-    bool Mapper003::ppuMapRead(uint16_t addr, uint32_t &mapped_addr, uint8_t &data)
+    bool Mapper003::ppuMapRead(uint16_t addr, uint32_t &mapped_addr, uint8_t &data, uint32_t systemClockCounter)
     {
         if (addr >= 0x0000 && addr <= 0x1FFF)
         {
@@ -44,7 +44,7 @@ namespace R2NES::Core
         return false;
     }
 
-    bool Mapper003::ppuMapWrite(uint16_t addr, uint32_t &mapped_addr, uint8_t data)
+    bool Mapper003::ppuMapWrite(uint16_t addr, uint32_t &mapped_addr, uint8_t data, uint32_t systemClockCounter)
     {
         if (addr >= 0x0000 && addr <= 0x1FFF)
         {
