7453ceba33b4b2694674061348834f55ee8a60d4
jpaulobf <jpaulobf@gmail.com>
2026-06-19 20:03:05 +0200

    New Engine

---
+++ src/System/AudioManager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SDL.h>
+#include <vector>
+
+namespace R2NES::System
+{
+    class AudioManager
+    {
+    public:
+        AudioManager();
+        ~AudioManager();
+
+        void initialize();
+        void close();
+
+        void pushSample(float sample);
+        void queueAudio(bool isFastForwarding);
+        
+        void clearQueuedAudio();
+
+        int getSampleRate() const { return sampleRate; }
+
+    private:
+        SDL_AudioDeviceID audioDevice = 0;
+        std::vector<float> audioBuffer;
+        int sampleRate = 44100;
+    };
+}
