e123c0c5e06911ee297d40d8cb18be793faab8a5
jpaulobf <jpaulobf@gmail.com>
2026-06-20 15:29:31 +0100

    Engine Improvements (System Package)

--- src/System/GameStateManager.h
+++ src/System/GameStateManager.h
@@ -2,9 +2,16 @@
 
 #include <string>
 #include <map>
+#include <SDL.h>
 
-namespace R2NES::Core { class NES; }
-namespace R2NES::Core { class Window; }
+namespace R2NES::Core
+{
+    class NES;
+}
+namespace R2NES::Core
+{
+    class Window;
+}
 
 namespace R2NES::System
 {
@@ -14,16 +21,16 @@ namespace R2NES::System
         GameStateManager() = default;
         ~GameStateManager() = default;
 
-        void loadRom(const std::string& path, Core::NES& nes, Core::Window& window);
-        void unloadRom(Core::NES& nes, Core::Window& window);
-        void reset(Core::NES& nes, Core::Window& window);
-        
-        void handleSaveLoadState(Core::NES& nes, Core::Window& window);
+        void loadRom(const std::string &path, Core::NES &nes, Core::Window &window);
+        void unloadRom(Core::NES &nes, Core::Window &window);
+        void reset(Core::NES &nes, Core::Window &window);
 
-        const std::map<uint16_t, std::string>& getCachedDisassembly() const { return cachedDisassembly; }
-        
+        void handleSaveLoadState(Core::NES &nes, Core::Window &window);
+
+        const std::map<uint16_t, std::string> &getCachedDisassembly() const { return cachedDisassembly; }
+
         // Permite recarregar o disassembly caso o banco de memória mude
-        void updateDisassemblyCache(Core::NES& nes, uint16_t currentPC);
+        void updateDisassemblyCache(Core::NES &nes, uint16_t currentPC);
 
     private:
         std::string currentRomPath;
