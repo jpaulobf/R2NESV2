47e0afa8bd1ccc344397bf63bd5c099d78d5b044
jpaulobf <jpaulobf@gmail.com>
2026-06-21 12:40:34 +0100

    Included control for the Zapper in Menu

--- src/Core/IO/Joysticks.h
+++ src/Core/IO/Joysticks.h
@@ -4,11 +4,19 @@
 
 namespace R2NES::Core::IO
 {
+    enum class DeviceType
+    {
+        Gamepad,
+        Zapper,
+        None
+    };
+
     class Joysticks
     {
     public:
         Controller controller1;
         Controller controller2;
+        DeviceType port2Device = DeviceType::Gamepad;
 
     public:
         Joysticks() {};
