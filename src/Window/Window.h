e832c6b3bcf6d6af1fc8811db240fcfe2fb3c585
jpaulobf <jpaulobf@gmail.com>
2026-07-19 20:43:24 +0100

    version

--- src/Window/Window.h
+++ src/Window/Window.h
@@ -407,7 +407,7 @@ namespace R2NES::Core
         bool unloadRequested = false;
         int width, height, scale;
 
-        std::string title = "R2NESV2 - build 0.8.5 | FPS: %.2f";
+        std::string title = "R2NESV2 - build 0.8.6 | FPS: %.2f";
         std::string romFile = "";
 
         // Valores Default Scanlines
